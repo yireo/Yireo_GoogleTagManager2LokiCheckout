7c9c851281c98855590cef8920690014151ec06b
Jisse Reitsma <jisse@yireo.com>
2025-05-06 11:47:38 +0200

    Add simple implementation

--- Component/CheckoutViewModel.php
+++ Component/CheckoutViewModel.php
@@ -3,8 +3,39 @@ declare(strict_types=1);
 
 namespace Yireo\GoogleTagManager2LokiCheckout\Component;
 
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Yireo\GoogleTagManager2\DataLayer\Event\AddPaymentInfo;
+use Yireo\GoogleTagManager2\DataLayer\Event\AddShippingInfo;
 use Yireo\LokiComponents\Component\ComponentViewModel;
 
 class CheckoutViewModel extends ComponentViewModel
 {
+    public function __construct(
+        private CheckoutSession $checkoutSession,
+        private AddShippingInfo $addShippingInfo,
+        private AddPaymentInfo $addPaymentInfo
+    ) {
+    }
+
+    public function getJsData(): array
+    {
+        return [
+            ...parent::getJsData(),
+            'shippingInfo' => $this->getShippingInfo(),
+            'paymentInfo' => $this->getPaymentInfo(),
+        ];
+    }
+
+    private function getShippingInfo(): array
+    {
+        return $this->addShippingInfo->get();
+    }
+
+    private function getPaymentInfo(): array
+    {
+        $this->addPaymentInfo->setCartId((int)$this->checkoutSession->getQuote()->getId());
+        $this->addPaymentInfo->setPaymentMethod((string)$this->checkoutSession->getQuote()->getPayment()->getMethod());
+
+        return $this->addPaymentInfo->get();
+    }
 }
