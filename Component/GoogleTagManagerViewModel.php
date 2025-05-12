86dce92a02a8194a61f1eafb2b8caae3922f6551
Jisse Reitsma <jisse@yireo.com>
2025-05-12 15:05:59 +0200

    WIP

--- Component/GoogleTagManagerViewModel.php
+++ Component/GoogleTagManagerViewModel.php
@@ -6,24 +6,37 @@ namespace Yireo\GoogleTagManager2LokiCheckout\Component;
 use Magento\Checkout\Model\Session as CheckoutSession;
 use Yireo\GoogleTagManager2\DataLayer\Event\AddPaymentInfo;
 use Yireo\GoogleTagManager2\DataLayer\Event\AddShippingInfo;
+use Yireo\GoogleTagManager2\DataLayer\Event\BeginCheckout;
 use Yireo\LokiComponents\Component\ComponentViewModel;
 
-class CheckoutViewModel extends ComponentViewModel
+class GoogleTagManagerViewModel extends ComponentViewModel
 {
     public function __construct(
         private CheckoutSession $checkoutSession,
+        private BeginCheckout $beginCheckout,
         private AddShippingInfo $addShippingInfo,
-        private AddPaymentInfo $addPaymentInfo
+        private AddPaymentInfo $addPaymentInfo,
     ) {
+    }
+
+    public function getJsComponentName(): ?string
+    {
+        return 'LokiCheckoutGoogleTagManager';
     }
 
     public function getJsData(): array
     {
         return [
             ...parent::getJsData(),
+            'beginCheckout' => $this->getBeginCheckoutInfo(),
             'shippingInfo' => $this->getShippingInfo(),
             'paymentInfo' => $this->getPaymentInfo(),
         ];
+    }
+
+    private function getBeginCheckoutInfo(): array
+    {
+        return $this->beginCheckout->get();
     }
 
     private function getShippingInfo(): array
